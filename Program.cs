5ed39f87b119449b313ed359be86df9e77cf9ed0
Joe Blanchette <jblanchette654@gmail.com>
2025-05-18 11:35:41 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
+using ModelContextProtocol.Server;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services
+       .AddMcpServer()
+       .WithStdioServerTransport()
+       .WithToolsFromAssembly();
+
+var host = builder.Build();
+await host.RunAsync();
+
+
+[McpServerToolType]
+public static class DotnetUtils
+{
+    [McpServerTool, Description("Gets the current time.")]
+    public static string GetCurrentTime() => DateTimeOffset.Now.ToString();
+    
+    [McpServerTool, Description("Generates a guid.")]
+    public static string GetGuid() => Guid.NewGuid().ToString();
+}
